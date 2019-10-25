9b650b2ce00e0edb759a76ec8e964f13b3494c49
Fakhir Shaheen <fakhir.shaheen@gmail.com>
2019-10-26 02:19:49 +0500

    Working with impossibly fast delegates. Success.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,14 @@
 
 #include <iostream>
+#include <vector>
 #include "FastDelegate/FastDelegate.h"
 #include "CppDelegates/Delegate.h"
 
 using namespace std;
 using namespace CppDelegates;
 using namespace fastdelegate;
+
+typedef CppDelegates::delegate<void (float)> EventDelegate;
 
 class GameObj{
     public:
@@ -14,11 +17,33 @@ class GameObj{
         }
 };
 
+class EventManager{
+    public:
+        EventManager(){}
+        ~EventManager(){}
+        
+        void AddListener(EventDelegate ed){
+            _list.push_back(ed);
+        }
+        
+        void Call(){
+            float f = 1.29f;
+            for(int i = 0; i < _list.size(); i++){
+                _list[i](f);
+                f += 0.23342;
+            }
+        }
+        
+    private:
+        vector<EventDelegate> _list;
+};
+
+
 int main( void )
 {
     GameObj obj1, obj2, obj3;
     
-
+/*
     typedef FastDelegate1<float> MyDelegate;
     MyDelegate newdeleg;
     newdeleg = MakeDelegate(&obj1, &GameObj::OnEvent);
@@ -31,6 +56,18 @@ int main( void )
     
     auto d1 = delegate<void(float)>::create<GameObj, &GameObj::OnEvent>(&obj3);
     d1(1.41);
+*/
+  
+    auto d1 = delegate<void(float)>::create<GameObj, &GameObj::OnEvent>(&obj1);
+    auto d2 = delegate<void(float)>::create<GameObj, &GameObj::OnEvent>(&obj2);
+    auto d3 = delegate<void(float)>::create<GameObj, &GameObj::OnEvent>(&obj3);
+    
+    EventManager emgr;
+    emgr.AddListener(d1);
+    emgr.AddListener(d2);
+    emgr.AddListener(d3);
+    
+    emgr.Call();
     
 	return 0;
 }
